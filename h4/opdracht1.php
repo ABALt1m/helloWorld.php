eaa8b085e70e6e94d7e0324d80ee41ce7a90b05f
Anas Bal <302587881@student.rocmondriaan.nl>
2022-09-01 12:52:36 +0200

    php opdracht 1 tot 3

--- h4/opdracht1.php
+++ h4/opdracht1.php
@@ -1,2 +1,33 @@
+<DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <title>
+            hello world
+        </title>
+    </head>
+    <body>
+     <?php
+     $tijdNu = date("H:i");
+     echo "$tijdNu";
+     if ($tijdNu<12 && $tijdNu>6){
+        echo "ochtend";
+     };
+     if ($tijdNu>= 12 && $tijdNu<18){
+       echo "middag";
+     };
+     if ($tijdNu>= 18 && $tijdNu<24){
+         echo "avond";
+     };
+
+
+
+
+
+
+
+
+     ?>
+    </body>
+    </html>
 <?php
-echo "hoi";
+
---
+++ h4/opdracht2.php
@@ -0,0 +1,27 @@
+<DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <title>
+            hello world
+        </title>
+    </head>
+    <body>
+     <?php
+     $todayTime = date("H");
+     echo "$todayTime";
+        switch($todayTime){
+            case $todayTime> 0 : echo "nachts"; break;
+        }
+
+
+
+
+
+
+
+
+
+     ?>
+    </body>
+    </html>
+<?php
